25ca0359231330d985f9d09415125630b7122b9d
divyabharathikopparapuvenkata <divikv219@gmail.com>
2022-06-22 17:40:28 +0000

    demo2

---
+++ jdbcconn.java
@@ -0,0 +1,40 @@
+package com.validatation;
+
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class jdbcconn {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		String qry = "select *from chart";
+			// Register for a class
+		Class.forName("com.mysql.jdbc.Driver");
+		//connection
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/chart", "root", "root");
+//		Statement
+		Statement statement = conn.createStatement();
+		//execute
+		 ResultSet rs= statement.executeQuery(qry);
+	while(rs.next())
+	{
+		System.out.println( rs.getString(1) );
+		
+	}
+	//if(rs.next())
+	//{
+//		System.out.println(rs.getInt(1) + " "+rs.getString(2) +"  " +rs.getString(3));
+	//}
+		}
+
+	
+
+
+	}
+
+
